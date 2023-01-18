03fd0f7f0bf86c83d2f54436ac82aef2bc018005
makim0939 <95673492+makim0939@users.noreply.github.com>
2023-01-18 13:49:45 +0900

    サーバを立ち上げるところまで

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 console.log("hello")
+
+//reqireはモジュールの読み込み
+const express = require("express")
+const app = express()
+const http = require("http")
+const server = http.createServer(app)
+const io = require("socket.io")(server)
+const PORT = 8000
+
+
+app.get("/", (req, res)=> {
+    res.send("hello")
+})
+
+server.listen(PORT, () =>{
+    console.log("listening on 8000")
+})
+
+
